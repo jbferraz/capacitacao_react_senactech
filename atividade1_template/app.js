ffa7236248b1b39c3229d82abb538dae58035c7c
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2022-06-03 18:47:04 -0300

    Listando 1 produto, Desafio, listar todos os produtos

---
+++ atividade1_template/app.js
@@ -0,0 +1,34 @@
+window.onload = trataOnLoad;
+/* Função: opção 1 */
+function trataOnLoad() {
+    buscaProdutos();
+    setInterval(buscaProdutos, 10000);
+}
+
+/* Função: opção 2 */
+const buscaProdutos = function () {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        const listaProdutos = JSON.parse(this.responseText);
+        console.log(listaProdutos[0]);
+        carregarProduto(listaProdutos[0])
+    }
+    xhttp.open("GET", "http://localhost:3000/produtos", true);
+    xhttp.send();
+}
+
+/* Função: opção 3 */
+const carregarProduto = (produto) => {
+    const htmlProduto = `
+        <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+            <div class="w3-card">
+                <div class="w3-container w3-center">
+                    <img src="${produto.imagem}" style="width: 70%">
+                        <h5>${produto.nome}</h5>
+                        <h3 class="w3-blue">${'RS ' + produto.preco.toLocaleString('pt-br'), {minimusFractioDigits: 2}}</h3>
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector('#produtos').innerHTML = htmlProduto;
+}
