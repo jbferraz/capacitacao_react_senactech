27d2bc491f03135c103582485202b50ca336fe5a
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2022-06-09 21:58:21 -0300

    App React

--- atividade1_template/app.js
+++ atividade1_template/app.js
@@ -10,25 +10,52 @@ const buscaProdutos = function () {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         const listaProdutos = JSON.parse(this.responseText);
-        console.log(listaProdutos[0]);
-        carregarProduto(listaProdutos[0])
+        console.log(listaProdutos);
+        carregarProduto(listaProdutos)
     }
     xhttp.open("GET", "http://localhost:3000/produtos", true);
     xhttp.send();
 }
 
 /* Função: opção 3 */
-const carregarProduto = (produto) => {
-    const htmlProduto = `
+const carregarProduto = (listaProdutos) => {
+    let htmlProduto = (listaProdutos.length > 0)
+        ? listaProdutos.map ((produto)=> {
+            return `
+            <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+                <div class="w3-card">
+                    <div class="w3-container w3-center">
+                        <img src="${produto.imagem}" style="width: 70%">
+                            <h5>${produto.nome}</h5>
+                            <h3 class="w3-blue">${'R$ ' + produto.preco.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</h3>
+                    </div>
+                </div>
+            </div>
+            `;
+        }).join(' ')
+        :'<h3>Não ha produtos na lista</h3>'
+    
+    document.querySelector('#produtos').innerHTML = htmlProduto;
+    /*
+    let htmlProduto = '';
+    if (listaProdutos.length > 0) {
+        for (let produto of listaProdutos) {
+            htmlProduto += `
         <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
             <div class="w3-card">
                 <div class="w3-container w3-center">
                     <img src="${produto.imagem}" style="width: 70%">
                         <h5>${produto.nome}</h5>
-                        <h3 class="w3-blue">${'RS ' + produto.preco.toLocaleString('pt-br'), {minimusFractioDigits: 2}}</h3>
+                        <h3 class="w3-blue">${'R$ ' + produto.preco.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</h3>
                 </div>
             </div>
         </div>
-    `;
+    `}
+    }
+    else {
+        htmlProduto += '<h3>Não ha produtos na lista</h3>'
+    };
+    
     document.querySelector('#produtos').innerHTML = htmlProduto;
+    */
 }
---
+++ react/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './components/Hello';
+import HiMessage from './components/HiMessage';
+
+function App() {
+  return (
+    <>
+      <Hello name='Fulano' idade={30}/>
+      <HiMessage message='Ciclano' />
+    </>
+  );
+}
+
+export default App;
