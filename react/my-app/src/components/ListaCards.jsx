a068d8f0db72ddbfd372deca909eb56f389acc0c
Jair Ferraz <jairbferraz@outlook.com>
2022-06-17 18:26:12 -0300

    ListaCards

--- react/my-app/src/components/ListaCards.jsx
+++ react/my-app/src/components/ListaCards.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import produtos from '../data/produtos';
 import Card from './MeuCard';
 
-function ListaCards(props){
-    return produtos.map (produto =>
+
+function ListaCards(props) {
+    return produtos.map (produto => 
         <MeuCard titulo={produto.nome}>
             <h3>{produto.preco}</h3>
-        </MeuCard>)
+        </MeuCard>    
+    )
 }
 
 export default ListaCards;
