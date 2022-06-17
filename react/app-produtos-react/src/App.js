f1abf49aa72d54d49ed77b88422af63d9dfcb662
Jair Ferraz <jairbferraz@outlook.com>
2022-06-17 19:08:00 -0300

    menu

--- react/app-produtos-react/src/App.js
+++ react/app-produtos-react/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Menu
 
+from './components/Menu';
 function App() {
   return (
-    <main>
-      <section clasName="w3-container w3-padding-16 w3-margin-top">
-        <h1>e-Commerce!!!</h1>
-      </section>
-    </main>
+    <>
+      <Menu></Menu>
+      <main>
+        <section clasName="w3-container w3-padding-16 w3-margin-top">
+          <h1>e-Commerce!!!</h1>
+        </section>
+      </main>
+      //<Carroussel></Carroussel>
+      //<ListaProdutos></ListaProdutos>
+      //<Footer></Footer>
+    </>
   );
 }
 
