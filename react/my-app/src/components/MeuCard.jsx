caeafcc4490d7f921905a50ebf786c1dc975966c
Jair Ferraz <jairbferraz@outlook.com>
2022-06-17 18:01:53 -0300

    MyCard

---
+++ react/my-app/src/components/MeuCard.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import './MeuCard.css'
+
+function MeuCard(props) {
+    return (
+        <div className="card">
+            <div className="card-titulo">
+                <h1>{props.titulo}</h1>
+            </div>
+            <div className="card-conteudo">
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default MeuCard;
